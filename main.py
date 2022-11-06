5af4a219737197fe8aab83e67d611ec7885e3585
Franco Borgiani <franco@nookalabs.com>
2022-11-06 20:57:27 -0300

    [ADD] gitignore and main.py

---
+++ main.py
@@ -0,0 +1,14 @@
+import argparse
+import logging
+logging.basicConfig(level=logging.INFO)
+
+logger = logging.getLogger()
+
+def main():
+  logger.info("Hello world at react \n")
+  
+  logger.info("Process finished")
+
+
+if __name__ == "__main__":
+  main()
