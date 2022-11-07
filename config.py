5464958716a7d9c674460fdd2cd46d0bb2822da1
Franco Borgiani <franco@nookalabs.com>
2022-11-06 22:33:23 -0300

    [ADD] coments to config.py & [FIX] config return

--- config.py
+++ config.py
@@ -7,7 +7,9 @@ def config():
   global __config
 
   if not __config:
+    # Read yaml file
     with open('config.yaml', mode="r") as f:
+      # Transform yaml to a pyhton dictonary
       __config = yaml.load(f, Loader=yaml.Loader)
 
-  return config
+  return __config
