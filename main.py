c8da7d210dd24e6f5dc31beec6aedc51197e5ad2
Franco Borgiani <franco@nookalabs.com>
2022-11-06 22:37:08 -0300

    add comments & args reader

--- main.py
+++ main.py
@@ -2,26 +2,27 @@ import argparse
 import logging
 logging.basicConfig(level=logging.INFO)
 
+from config import config
+
 logger = logging.getLogger()
 
 def main():
-  logger.info("Hello world at react \n")
-
-
-
-  logger.info("Process finished")
-
+  logger.info("Starting web scrapper")
 
 if __name__ == "__main__":
   # Instance arg parser
   parser = argparse.ArgumentParser()
 
+  #  Get News Sites Choices from the config
+  news_sites_choices = list(config()['news_sites'].keys())
+
   # Request Arguments in the prompt
-  parser.add_argument('filename',
-                      help='The path to the dirty data',
+  parser.add_argument('news_site_uid',
+                      help="The news site that you want to scrap",
+                      choices=news_sites_choices,
                       type=str)
 
   # Get parsed arguments
-  arg = parser.parse_args() 
-
+  args = parser.parse_args() 
+  
   main()
