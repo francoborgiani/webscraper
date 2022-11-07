e5b4d52dc40bbb9d6394c5c2d494a06b8f68e535
Franco Borgiani <franco@nookalabs.com>
2022-11-06 21:16:42 -0300

    [ADD] installation guide & requirements.txt

--- main.py
+++ main.py
@@ -6,9 +6,22 @@ logger = logging.getLogger()
 
 def main():
   logger.info("Hello world at react \n")
-  
+
+
+
   logger.info("Process finished")
 
 
 if __name__ == "__main__":
+  # Instance arg parser
+  parser = argparse.ArgumentParser()
+
+  # Request Arguments in the prompt
+  parser.add_argument('filename',
+                      help='The path to the dirty data',
+                      type=str)
+
+  # Get parsed arguments
+  arg = parser.parse_args() 
+
   main()
---
+++ readme.md
@@ -0,0 +1,13 @@
+# Installation guide
+
+Python Version (3.8)
+```
+python -m venv venv
+```
+
+Install dependencies
+```
+pip install -r requirements.txt
+```
+
+If you found issues, please send it.
